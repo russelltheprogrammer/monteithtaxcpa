83341761301eaa83643b740c746235ee45eb17e3
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-12-09 16:01:02 -0500

    design changes

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <div className="text-blurbs">
                 <br/>
                 <h1>Welcome to RUSSELL MONTEITH CPA PLLC</h1>
-                <h4>A digital accounting practice</h4>
+                <h4>A digital tax accounting practice</h4>
                 <br/>
                 <img id="homepage-logo-img" src="homepagelogoimage.png" alt="Home Page Logo" />
                 <br/>
