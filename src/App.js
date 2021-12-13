653905c1b1860b7593f0d7887073792ad24c5f8f
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-12-13 16:52:01 -0500

    sass updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './index.scss';
-import "./style.scss";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
