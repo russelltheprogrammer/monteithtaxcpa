c7ce7f827a8a27e3ba279ce8bf138b5262142c39
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-12-22 14:19:06 -0500

    add node-env if stmt

--- server/server.js
+++ server/server.js
@@ -80,11 +80,11 @@ router.post("/api/", (req, res) => {
 });
 
 
-// if (process.env.NODE_ENV === 'production') {
-//     // Serve any static files
-//     app.use(express.static(path.join(__dirname, 'client/build')));
-//   // Handle React routing, return all requests to React app
-//     app.get('*', function(req, res) {
-//       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//     });
-//   }
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
