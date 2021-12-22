ce57ff2ab09cb88f5db82ccf2864098db450d121
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-12-22 15:46:39 -0500

    change server port

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const dotenv = require('dotenv');
 const path = require('path');
 dotenv.config();
 
-const port = process.env.SERVERPORT;
 const userName = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const emailService = process.env.EMAILSERVICE;
@@ -35,7 +34,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(port, () => console.log("Server Running"));
+app.listen(process.env.PORT || 3001, () => console.log("Server Running"));
  
 const contactEmail = nodemailer.createTransport({
     service: emailService,
