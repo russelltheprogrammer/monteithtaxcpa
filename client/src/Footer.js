ddd9e4750772a66331fc96f2f53d58e8f24a82b5
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-02-02 15:39:45 -0500

    update footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     return ( 
         <div className="footer-container">
             <div className="gradient-container-bottom"></div>
-            <i className="fas fa-copyright"></i>Copyright 2021 - All Rights Reserved - RUSSELL MONTEITH CPA PLLC
+            <i className="fas fa-copyright"></i>Copyright 2022 - All Rights Reserved - RUSSELL MONTEITH CPA PLLC
         </div>
      );
 }
